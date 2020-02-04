832d28c195da11fb2a14826051336f54c76df04c
NikitaMasl <nikitamas1998@mail.ru>
2020-02-04 22:43:07 +0300

    beautify

--- HW4-MaslovNikita/script/script.js
+++ HW4-MaslovNikita/script/script.js
@@ -1,125 +1,129 @@
-
 //Make your order
 var order = {
-    bigburgers:{
-        quontity:0,
-        cheese:0,
-        salad:0,
-        potatoes:0
+    bigburgers: {
+        quontity: 1,
+        cheese: 2,
+        salad: 0,
+        potatoes: 4
     },
-    smallburgers:{
-        quontity:0,
-        cheese:0,
-        salad:0,
-        potatoes:0
+    smallburgers: {
+        quontity: 3,
+        cheese: 0,
+        salad: 0,
+        potatoes: 0
     },
-    olivie:0,
-    cesar:0,
-    cola:0,
-    coffe:0
+    olivie: 700,
+    cesar: 700,
+    cola: 2,
+    coffe: 2
 };
 
-function Burger(){
-    
+function Burger() {
+
 }
-Burger.prototype.add_cheese = function(num){
-    this.price += (10*num);
-    this.cal += (20*num);
+Burger.prototype.add_cheese = function (num) {
+    this.price += (10 * num);
+    this.cal += (20 * num);
 }
-Burger.prototype.add_salad = function(num){
-    this.price += (20*num);
-    this.cal += (5*num);
+Burger.prototype.add_salad = function (num) {
+    this.price += (20 * num);
+    this.cal += (5 * num);
 }
-Burger.prototype.add_potatoes = function(num){
-    this.price += (15*num);
-    this.cal += (10*num);
+Burger.prototype.add_potatoes = function (num) {
+    this.price += (15 * num);
+    this.cal += (10 * num);
 }
-function Big_burger(){
-    this.price =  100;
+
+function Big_burger() {
+    this.price = 100;
     this.cal = 40;
 }
 Big_burger.prototype = Object.create(Burger.prototype)
-function Small_burger(){
-    this.price =  50;
+
+function Small_burger() {
+    this.price = 50;
     this.cal = 20;
 }
 Small_burger.prototype = Object.create(Burger.prototype)
 
 
-function Salad(){
+function Salad() {
 
 }
-Salad.prototype.calculateprice = function(weight){
-    this.price = ((weight*this.price)/100); 
-    this.cal = ((weight*this.cal)/100); 
+Salad.prototype.calculateprice = function (weight) {
+    this.price = ((weight * this.price) / 100);
+    this.cal = ((weight * this.cal) / 100);
 }
-function Cesar(){
+
+function Cesar() {
     this.price = 100;
     this.cal = 20;
 }
 Cesar.prototype = Object.create(Salad.prototype);
-function Olivie(){
+
+function Olivie() {
     this.price = 50;
     this.cal = 80;
 }
 Olivie.prototype = Object.create(Salad.prototype);
 
 
-function Cola(){
+function Cola() {
     this.price = 50;
     this.cal = 40;
 }
-function Coffe(){
+
+function Coffe() {
     this.price = 80;
     this.cal = 20;
 }
 
-function makeorder(){
-    var bill=[];
+function makeorder() {
+    var bill = [];
     var money = 0;
-    var cal=0;
-    for(i=0; i<order.bigburgers.quontity; i++){
-        bill[i]=new Big_burger();
+    var cal = 0;
+    for (i = 0; i < order.bigburgers.quontity; i++) {
+        bill[i] = new Big_burger();
         bill[i].add_cheese(order.bigburgers.cheese);
         bill[i].add_salad(order.bigburgers.salad);
         bill[i].add_potatoes(order.bigburgers.potatoes);
     }
-    for(i=order.bigburgers.quontity; i<order.smallburgers.quontity+order.bigburgers.quontity; i++){
-        bill[i]=new Small_burger();
+    for (i = order.bigburgers.quontity; i < order.smallburgers.quontity + order.bigburgers.quontity; i++) {
+        bill[i] = new Small_burger();
         bill[i].add_cheese(order.smallburgers.cheese);
         bill[i].add_salad(order.smallburgers.salad);
         bill[i].add_potatoes(order.smallburgers.potatoes);
     }
-    var numprevpos=order.bigburgers.quontity+order.smallburgers.quontity;
-    if(order.olivie>0){
+    var numprevpos = order.bigburgers.quontity + order.smallburgers.quontity;
+    if (order.olivie > 0) {
         bill[numprevpos] = new Olivie();
-        bill[numprevpos] .calculateprice(order.olivie);
+        bill[numprevpos].calculateprice(order.olivie);
         numprevpos++;
     }
-    if(order.cesar>0){
+    if (order.cesar > 0) {
         bill[numprevpos] = new Cesar();
-        bill[numprevpos] .calculateprice(order.cesar);
+        bill[numprevpos].calculateprice(order.cesar);
         numprevpos++;
     }
-    if(order.cola>0){
-        for(i=numprevpos; i<numprevpos+order.cola; i++){
+    if (order.cola > 0) {
+        for (i = numprevpos; i < numprevpos + order.cola; i++) {
             bill[i] = new Cola();
         }
-        numprevpos+=order.cola;
+        numprevpos += order.cola;
     }
-    if(order.coffe>0){
-        for(i=numprevpos; i<numprevpos+order.coffe; i++){
+    if (order.coffe > 0) {
+        for (i = numprevpos; i < numprevpos + order.coffe; i++) {
             bill[i] = new Coffe();
         }
-        numprevpos+=order.coffe;
+        numprevpos += order.coffe;
     }
     console.log("Your bill:")
-    for(i=0;i<bill.length;i++){
+    for (i = 0; i < bill.length; i++) {
         console.log(bill[i]);
-        money+=bill[i].price;
-        cal+=bill[i].cal
+        money += bill[i].price;
+        cal += bill[i].cal
     }
     console.log("Your order cost: " + money + " tugrikov");
-    console.log("And have "+cal+" cal");
+    console.log("And have " + cal + " cal");
 }
 makeorder();
